8e34465ba82b7ef83b2b50a6955e1e8ba6dd2dfc
jjdelvalle <delvallejacobo@gmail.com>
2021-03-14 22:08:57 -0600

    Fix dataset generation

--- app/prep_dataset.py
+++ app/prep_dataset.py
@@ -21,23 +21,28 @@ def main():
             audio_fname = join(DATA_PATH, grid_file[:grid_file.index('.')] + '.wav')
             audio_file = AudioSegment.from_wav(audio_fname)
             audio_file = audio_file.set_frame_rate(16000).set_channels(1)
-            init_t = 0
-            end_t = 0
+            init_t = None
+            end_t = None
             text = ""
             for line in f:
                 if 'xmin' in line:
                     init_t = float(time_regex.search(line).group(0)) * 1000
                 elif 'xmax' in line:
                     end_t = float(time_regex.search(line).group(0)) * 1000
-                elif 'text' in line:
+                elif 'text = ' in line and init_t is not None and end_t is not None:
                     text = line[(line.index('=') + 3):-2]
                     if len(text) < 5:
                         continue
                     temp_audio = audio_file[floor(init_t):ceil(end_t)]
                     filename = f"{grid_file[:(grid_file.index('.'))]}_{floor(init_t // 1000)}.wav"
-                    temp_audio = temp_audio.export("../data/processed/" + filename, format="wav")
+                    temp_audio.export("../data/processed/" + filename, format="wav")
                     file_dict.append({'file': "../data/processed/" + filename, 'text': text})
-
+                    init_t = None
+                    end_t = None
+                else:
+                    init_t = None
+                    end_t = None
+                    text = None
 
     df = pd.DataFrame(file_dict, columns=['file', 'text'])
     df['text'] = df['text'].str.strip(', "')
